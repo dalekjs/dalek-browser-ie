f06c1bbe44b9dc6e364b71cb6833f1d48d780475
asciidisco <public@asciidisco.com>
2013-06-21 21:24:31 +0200

    Linting errors

--- index.js
+++ index.js
@@ -22,6 +22,8 @@
  *
  */
 
+'use strict';
+
 // ext. libs
 var Q = require('q');
 var spawn = require('child_process').spawn;
@@ -36,127 +38,127 @@ var iedriver = require('./lib/iedriver');
 
 module.exports = {
 
-    /**
-     * Verbose version of the browser name
-     *
-     * @property
-     * @type string
-     * @default Internet Explorer
-     */
+  /**
+   * Verbose version of the browser name
+   *
+   * @property
+   * @type string
+   * @default Internet Explorer
+   */
 
-    longName: 'Internet Explorer',
+  longName: 'Internet Explorer',
 
-    /**
-     * Default port of the IEDriverServer
-     * The port may change, cause the port conflict resultion
-     * tool might pick another one, if the default one is blocked
-     *
-     * @property
-     * @type integer
-     * @default 5555
-     */
+  /**
+   * Default port of the IEDriverServer
+   * The port may change, cause the port conflict resultion
+   * tool might pick another one, if the default one is blocked
+   *
+   * @property
+   * @type integer
+   * @default 5555
+   */
 
-    port: 5555,
+  port: 5555,
 
-    /**
-     * Default host of the IEDriverServer
-     * The host may be overriden with
-     * a user configured value
-     *
-     * @property
-     * @type string
-     * @default localhost
-     */
+  /**
+   * Default host of the IEDriverServer
+   * The host may be overriden with
+   * a user configured value
+   *
+   * @property
+   * @type string
+   * @default localhost
+   */
 
-    host: 'localhost',
+  host: 'localhost',
 
-    /**
-     * Path to the IEDriverServer.exe file
-     *
-     * @property
-     * @type string
-     * @default /
-     */
+  /**
+   * Path to the IEDriverServer.exe file
+   *
+   * @property
+   * @type string
+   * @default /
+   */
 
-    path: '/',
+  path: '/',
 
-    /**
-     * Child process instance of the IEDriverServer
-     *
-     * @property
-     * @type null|Object
-     */
+  /**
+   * Child process instance of the IEDriverServer
+   *
+   * @property
+   * @type null|Object
+   */
 
-    spawned: null,
+  spawned: null,
 
-    /**
-     * Resolves the driver port
-     *
-     * @method getPort
-     * @return integer
-     */
+  /**
+   * Resolves the driver port
+   *
+   * @method getPort
+   * @return integer
+   */
 
-    getPort: function () {
-        return this.port;
-    },
+  getPort: function () {
+    return this.port;
+  },
 
-    /**
-     * Returns the driver host
-     *
-     * @method getHost
-     * @type string
-     */
+  /**
+   * Returns the driver host
+   *
+   * @method getHost
+   * @type string
+   */
 
-    getHost: function () {
-        return this.host;
-    },
+  getHost: function () {
+    return this.host;
+  },
 
-    /**
-     * Launches the driver
-     * (the driver takes care of launching the browser)
-     *
-     * @method launch
-     * @return Q.promise
-     */
+  /**
+   * Launches the driver
+   * (the driver takes care of launching the browser)
+   *
+   * @method launch
+   * @return Q.promise
+   */
 
-    launch: function (options) {
-        var deferred = Q.defer();
-        var stream = '';
-        this.spawned = spawn(iedriver.path, ['--port=' + this.getPort(), '--host=' + this.getHost()]);
+  launch: function () {
+    var deferred = Q.defer();
+    var stream = '';
+    this.spawned = spawn(iedriver.path, ['--port=' + this.getPort(), '--host=' + this.getHost()]);
 
-        this.spawned.stdout.on('data', function (data) {
-            var dataStr = new String(data);
-            stream += dataStr;
-            if (stream.search('Listening on port') !== -1) {
-                deferred.resolve();
-            }
+    this.spawned.stdout.on('data', function (data) {
+      var dataStr = data + '';
+      stream += dataStr;
+      if (stream.search('Listening on port') !== -1) {
+        deferred.resolve();
+      }
+    });
+    return deferred.promise;
+  },
+
+  /**
+   * Kills the driver & browser processes
+   *
+   * @method kill
+   * @chainable
+   */
+
+  kill: function () {
+    // get a list of all running processes
+    nwin.list(function(svc){
+      // filter out the browser process
+      svc.forEach(function (item, idx) {
+        Object.keys(item).forEach(function (key) {
+          if(svc[idx][key] === 'iexplore.exe') {
+            // kill the browser process
+            nwin.kill(svc[idx].PID);
+          }
         });
-        return deferred.promise;
-    },
+      });
+    },true);
 
-    /**
-     * Kills the driver & browser processes
-     *
-     * @method kill
-     * @chainable
-     */
-
-    kill: function () {
-        // get a list of all running processes
-        nwin.list(function(svc){
-          // filter out the browser process
-          svc.forEach(function (item, idx) {
-            Object.keys(item).forEach(function (key) {
-              if(svc[idx][key] === 'iexplore.exe') {
-                // kill the browser process
-                nwin.kill(svc[idx].PID);
-              }
-            });
-          });
-        },true);
-
-        // kill the driver process
-        this.spawned.kill('SIGTERM');
-        return this;
-    }
+    // kill the driver process
+    this.spawned.kill('SIGTERM');
+    return this;
+  }
 };
--- install.js
+++ install.js
@@ -1,197 +1,189 @@
 // Shamelessly copied from The Obvious Corporation
 
 /*
- * This simply fetches the right version of iedriver for the current platform.
+ * This simply fetches the right version of iedriver
  */
 
-'use strict'
+'use strict';
 
-var cp = require('child_process')
-var fs = require('fs')
-var http = require('http')
-var kew = require('kew')
-var path = require('path')
-var url = require('url')
-var rimraf = require('rimraf').sync
-var AdmZip = require('adm-zip')
-var helper = require('./lib/iedriver')
-var ncp = require('ncp')
-var npmconf = require('npmconf')
-var util = require('util')
+var cp = require('child_process');
+var fs = require('fs');
+var http = require('http');
+var kew = require('kew');
+var path = require('path');
+var url = require('url');
+var rimraf = require('rimraf').sync;
+var AdmZip = require('adm-zip');
+var helper = require('./lib/iedriver');
+var ncp = require('ncp');
+var npmconf = require('npmconf');
+var util = require('util');
 
-var libPath = path.join(__dirname, 'lib', 'bin', 'iedriver')
-var downloadUrl = 'http://selenium.googlecode.com/files/IEDriverServer_'
+var libPath = path.join(__dirname, 'lib', 'bin', 'iedriver');
+var downloadUrl = 'http://selenium.googlecode.com/files/IEDriverServer_';
 
 if (process.platform === 'win32') {
-  downloadUrl += 'Win32_2.33.0.zip'
+  downloadUrl += 'Win32_2.33.0.zip';
 } else {
-  console.log('Unexpected platform or architecture:', process.platform, process.arch)
-  process.exit(1)
+  console.log('Unexpected platform or architecture:', process.platform, process.arch);
+  process.exit(1);
 }
 
-var fileName = downloadUrl.split('/').pop()
-
-
-npmconf.load(function(err, conf) {
-  if (err) {
-    console.log('Error loading npm config')
-    console.error(err)
-    process.exit(1)
-    return
-  }
-
-  var tmpPath = path.join(conf.get('tmp'), 'iedriver')
-  var downloadedFile = path.join(tmpPath, fileName)
-
-  var promise= kew.resolve(true)
-
-  // Start the install.
-  if (!fs.existsSync(downloadedFile)) {
-    promise = promise.then(function () {
-      rimraf(tmpPath)
-      mkdir(downloadedFile)
-      console.log('Downlaoading', downloadUrl)
-      console.log('Saving to', downloadedFile)
-      return requestBinary(getRequestOptions(conf.get('proxy')), downloadedFile)
-    })
-
-  } else {
-    console.log('Download already available at', downloadedFile)
-  }
-
-  promise.then(function () {
-    return extractDownload(downloadedFile, tmpPath)
-  })
-  .then(function () {
-    return copyIntoPlace(tmpPath, libPath)
-  })
-  .then(function () {
-    return fixFilePermissions(tmpPath, libPath)
-  })
-  .then(function () {
-    console.log('Done. IEDriver binary available at', helper.path)
-  })
-  .fail(function (err) {
-    console.error('IEDriver installation failed', err.stack)
-    process.exit(1)
-  })
-})
-
+var fileName = downloadUrl.split('/').pop();
 
 function getRequestOptions(proxyUrl) {
   if (proxyUrl) {
-    var options = url.parse(proxyUrl)
-    options.path = downloadUrl
-    options.headers = { Host: url.parse(downloadUrl).host }
+    var options = url.parse(proxyUrl);
+    options.path = downloadUrl;
+    options.headers = { Host: url.parse(downloadUrl).host };
     // turn basic authorization into proxy-authorization
     if (options.auth) {
-      options.headers['Proxy-Authorization'] = 'Basic ' + new Buffer(options.auth).toString('base64')
-      delete options.auth
+      options.headers['Proxy-Authorization'] = 'Basic ' + new Buffer(options.auth).toString('base64');
+      delete options.auth;
     }
 
-    return options
+    return options;
   } else {
-    return url.parse(downloadUrl)
+    return url.parse(downloadUrl);
   }
 }
-
 
 function requestBinary(requestOptions, filePath) {
-  var deferred = kew.defer()
+  var deferred = kew.defer();
 
-  var count = 0
-  var notifiedCount = 0
-  var outFile = fs.openSync(filePath, 'w')
+  var count = 0;
+  var notifiedCount = 0;
+  var outFile = fs.openSync(filePath, 'w');
   var client = http.get(requestOptions, function (response) {
-    var status = response.statusCode
-    console.log('Receiving...')
+    var status = response.statusCode;
+    console.log('Receiving...');
     if (status === 200) {
       response.addListener('data',   function (data) {
-        fs.writeSync(outFile, data, 0, data.length, null)
-        count += data.length
+        fs.writeSync(outFile, data, 0, data.length, null);
+        count += data.length;
         if ((count - notifiedCount) > 800000) {
-          console.log('Received ' + Math.floor(count / 1024) + 'K...')
-          notifiedCount = count
+          console.log('Received ' + Math.floor(count / 1024) + 'K...');
+          notifiedCount = count;
         }
-      })
+      });
 
       response.addListener('end',   function () {
-        console.log('Received ' + Math.floor(count / 1024) + 'K total.')
-        fs.closeSync(outFile)
-        deferred.resolve(true)
-      })
+        console.log('Received ' + Math.floor(count / 1024) + 'K total.');
+        fs.closeSync(outFile);
+        deferred.resolve(true);
+      });
 
     } else {
-      client.abort()
-      deferred.reject('Error with http request: ' + util.inspect(response.headers))
+      client.abort();
+      deferred.reject('Error with http request: ' + util.inspect(response.headers));
     }
-  })
+  });
 
-  return deferred.promise
+  return deferred.promise;
 }
 
-
 function extractDownload(filePath, tmpPath) {
-  var deferred = kew.defer()
-  var options = {cwd: tmpPath}
+  var deferred = kew.defer();
+  var options = {cwd: tmpPath};
 
-  rimraf(libPath)
+  rimraf(libPath);
 
   if (filePath.substr(-4) === '.zip') {
-    console.log('Extracting zip contents')
+    console.log('Extracting zip contents');
     try {
-      var zip = new AdmZip(filePath)
-      zip.extractAllTo(path.dirname(filePath), true)
-      deferred.resolve(true)
+      var zip = new AdmZip(filePath);
+      zip.extractAllTo(path.dirname(filePath), true);
+      deferred.resolve(true);
     } catch (e) {
       console.log(e);
-      deferred.reject('Error extracting archive ' + e.stack)
+      deferred.reject('Error extracting archive ' + e.stack);
     }
 
   } else {
-    console.log('Extracting tar contents (via spawned process)')
-    cp.execFile('tar', ['jxf', filePath], options, function (err, stdout, stderr) {
+    console.log('Extracting tar contents (via spawned process)');
+    cp.execFile('tar', ['jxf', filePath], options, function (err) {
       if (err) {
-        deferred.reject('Error extracting archive ' + err.stack)
+        deferred.reject('Error extracting archive ' + err.stack);
       } else {
-        deferred.resolve(true)
+        deferred.resolve(true);
       }
-    })
+    });
   }
-  return deferred.promise
+  return deferred.promise;
 }
 
-
 function copyIntoPlace(tmpPath, targetPath) {
-  var deferred = kew.defer()
+  var deferred = kew.defer();
   // Look for the extracted directory, so we can rename it.
-  var files = fs.readdirSync(tmpPath)
+  var files = fs.readdirSync(tmpPath);
   for (var i = 0; i < files.length; i++) {
-    var file = path.join(tmpPath, files[i])
+    var file = path.join(tmpPath, files[i]);
     file = file;
     if (fs.statSync(file).isFile() && file.search('zip') === -1) {
       targetPath = targetPath.replace('iedriver', 'IEDriverServer');
       try {
-        ncp(file, targetPath, deferred.makeNodeResolver())
+        ncp(file, targetPath, deferred.makeNodeResolver());
       } catch (e) {
 
       }
-      break
+      break;
     }
   }
-  return deferred.promise
+  return deferred.promise;
 }
-
-
 
 function fixFilePermissions(tmpPath, libPath) {
-  fs.renameSync(libPath.replace('iedriver', 'IEDriverServer'), libPath.replace('iedriver', 'IEDriverServer.exe'))
+  fs.renameSync(libPath.replace('iedriver', 'IEDriverServer'), libPath.replace('iedriver', 'IEDriverServer.exe'));
 }
-
 
 function mkdir(name) {
-  var dir = path.dirname(name)
+  var dir = path.dirname(name);
   if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir)
+    fs.mkdirSync(dir);
   }
 }
+
+npmconf.load(function(err, conf) {
+  if (err) {
+    console.log('Error loading npm config');
+    console.error(err);
+    process.exit(1);
+    return;
+  }
+
+  var tmpPath = path.join(conf.get('tmp'), 'iedriver');
+  var downloadedFile = path.join(tmpPath, fileName);
+
+  var promise= kew.resolve(true);
+
+  // Start the install.
+  if (!fs.existsSync(downloadedFile)) {
+    promise = promise.then(function () {
+      rimraf(tmpPath);
+      mkdir(downloadedFile);
+      console.log('Downlaoading', downloadUrl);
+      console.log('Saving to', downloadedFile);
+      return requestBinary(getRequestOptions(conf.get('proxy')), downloadedFile);
+    });
+
+  } else {
+    console.log('Download already available at', downloadedFile);
+  }
+
+  promise.then(function () {
+    return extractDownload(downloadedFile, tmpPath);
+  })
+  .then(function () {
+    return copyIntoPlace(tmpPath, libPath);
+  })
+  .then(function () {
+    return fixFilePermissions(tmpPath, libPath);
+  })
+  .then(function () {
+    console.log('Done. IEDriver binary available at', helper.path);
+  })
+  .fail(function (err) {
+    console.error('IEDriver installation failed', err.stack);
+    process.exit(1);
+  });
+});
